5b2f90259fb14337db7deeebdd37a79b41630a0e
dnguyen2065 <dnguyen2065@gmail.com>
2022-11-29 23:30:23 -0500

    first question done

--- src/App.java
+++ src/App.java
@@ -10,11 +10,21 @@ public class App {
          * despite the fact that x = (1, 2, 3) or x1 = 1, x2 = 2, x3 = 3 can be easily
          * verified as a solution to the system.
          * ANSWER: This system of equations cannot be solved by normal
-         * ForwardElimination because it could be distorted by rounding errors
+         * ForwardElimination because it could be distorted by rounding errors from when
+         * the scaling factor gets so large that the difference between numbers of great
+         * magnitude
+         * creates the possibility of massive rounding error.
          * 
          * 
          * How does the BetterForwardElimination algorithm on page 211 of Levitin rem-
          * edy this?
+         * ANSWER: BetterForwardElimination solves this problem by implementing partial
+         * pivoting.
+         * partial pivoting works by looking for a row with the largest absolute value
+         * of
+         * the coefficient in the ith column and swap it with the ith row then use the
+         * resultant matrix
+         * as the ith iteraiton pivot.
          * 
          */
     }
