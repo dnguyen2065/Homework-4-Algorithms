0215db8fe7111cd87581d95492830b5b6eb39bf3
dnguyen2065 <dnguyen2065@gmail.com>
2022-11-29 22:15:30 -0500

    second commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        /*
+         * Question 1
+         * (5 Points) Explain why the (repaired1)ForwardElimination algorithm on page
+         * 210 of Levitin fails to provide a solution for:
+         * x1 + x2 + x3 = 6
+         * x1 + x2 + 2x3 = 9
+         * x1 + 2x2 + 3x3 = 14
+         * despite the fact that x = (1, 2, 3) or x1 = 1, x2 = 2, x3 = 3 can be easily
+         * verified as a solution to the system.
+         * ANSWER: This system of equations cannot be solved by normal
+         * ForwardElimination because it could be distorted by rounding errors
+         * 
+         * 
+         * How does the BetterForwardElimination algorithm on page 211 of Levitin rem-
+         * edy this?
+         * 
+         */
     }
 }
